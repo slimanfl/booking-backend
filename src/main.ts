af9600ab023784ecfdfeb68c2664965f03f35e2c
slimanfl <slimanite1996@gmail.com>
2024-10-16 18:50:57 -0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { hostname } from 'os';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, '127.0.0.1');
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
