56b74202e345cabc0f7fd7478843a985cbd0ec15
slimanfl <slimanite1996@gmail.com>
2024-10-27 16:31:47 -0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,9 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { GateWayModule } from './gateway/gateway.module';
 import { EventsModule } from './events/events.module';
 @Module({
-  imports: [
-    ConfigModule,
-    GateWayModule,
-    DatabaseModule,
-    UsersModule,
-    EventsModule,
-  ],
+  imports: [ConfigModule, DatabaseModule, UsersModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
