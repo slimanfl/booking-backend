84ce69cb4753837c376350824317e26c1a2fd880
slimanfl <slimanite1996@gmail.com>
2024-10-28 06:10:23 -0700

    test3

--- dist/events/interfaces/event.interface.d.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from 'mongoose';
-export interface Event extends Document {
-    readonly count: number;
-    readonly meals: string;
-    readonly section: string;
-    readonly date: Date;
-    readonly userId: string;
-}
--- dist/users/dto/Admin/update-user.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { createUserDto } from './create-user.dto';
-declare const updateUserDto_base: import("@nestjs/mapped-types").MappedType<Partial<createUserDto>>;
-export declare class updateUserDto extends updateUserDto_base {
-}
-export {};
